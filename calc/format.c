c79ed482d4b47ffa6503047bdaeaac0af59b9f19
Stewart <stewartlsmith12@gmail.com>
2013-03-04 22:23:00 -0500

    NEW ALGO

--- calc/format.c
+++ calc/format.c
@@ -47,6 +47,11 @@ int neg_b2d(char bin[]){
   return ans;
 }
 
+typedef union {
+  int integer;
+  float flt;
+}Number;
+
 /*Main Method */
 void main(int argc, char*argv[]){
 
@@ -56,31 +61,76 @@ void main(int argc, char*argv[]){
       return;
     }
 
-    /*Check if agrv[2] is int or float */
-    int integer = 0;
-    int floater = 0;
-    if(strcmp(argv[2] , "int") == 0)
-      integer = 1;
-    else{
-      if(strcmp(argv[2] , "float") == 0)
-        floater = 1;
-      else fprintf(stderr, "Can only use float and int");
+    /*check if binary num is 32 bits */
+    if(strlen(argv[1]) != 32){
+        fprintf(stderr, "ERROR: Binary number must be 32 bits\n");
+        return;
     }
 
     char* binary = argv[1];
+    /*Check if agrv[2] is int or float */
+    Number output;
+    if(strcmp(argv[2] , "int") == 0 || strcmp(argv[2] , "float") == 0){
+     while (*binary){
+      output.integer = (output.integer << 1) | (*binary == '1');
+      binary ++;
+     }
+    }
+    else{
+        fprintf(stderr, "ERROR: Can only use float and int\n");
+        return;
+      }
+   if( strcmp(argv[2] , "int") == 0){ 
+     printf("output %d\n", output.integer);
+   }
+   else{
+     printf("output %f\n", output.flt);
+   }
+    
     /*check if binary number is negative */
     int negative = 0;
     if(binary[strlen(binary)-1] == '1'){
       negative = 1;
     }
-
-    int ans;
-    if(negative){
-      ans = neg_b2d(binary);
-    }
-    else{
-    ans = b2d(binary);
-    }
-    printf("Answer: %d\n", ans);
 }
+
+/*
+    int ans;
+    int mantissa;
+    int exponent;
+    if(integer){  binary num is integer
+      if(negative){
+        ans = neg_b2d(binary);
+      }
+      else{
+        ans = b2d(binary);
+      }
+      printf("Answer: %d\n", ans);
+    }
+    else if(floater){  binary num is float 
+     char* mant = (char*) malloc(23);
+     char* expon = (char*) malloc(23);
+     strcpy(mant, "");
+     strcpy(expon, "");
+     char test[9];
+     int k;
+     int b;
+     for(k=0; k<23; k++){
+       char temp = binary[k];
+       strcat(mant, &temp);
+     }
+     int a=0;
+     for(b=23; b<31; b++){
+       test[a] = binary[b];
+       a++;
+     }
+     test[a] = '\0';
+     strcat(expon, test); 
+
+     exponent = b2d(expon)-127;
+     mantissa = b2d(mant);
+    }
+  printf("exponent: %d\n", exponent);
+  printf("mantissa: %d\n", mantissa);
+*/
 
